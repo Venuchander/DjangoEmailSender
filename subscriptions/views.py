9eb956f7d2d9d4f14eaba3de8cf64f3aad54c97e
Venuchander B <106867270+Venuchander@users.noreply.github.com>
2024-05-31 17:55:29 +0530

    Add files via upload

---
+++ subscriptions/views.py
@@ -0,0 +1,33 @@
+from django.contrib import messages
+from django.core.mail import EmailMessage
+from django.shortcuts import render, redirect
+from django.conf import settings
+import os
+
+def subscribe(request):
+    if request.method == 'POST':
+        subject = 'Python (Selenium) Assignment - Venuchander'
+        message = 'Sending Email through Gmail'
+        recipient = 'tech@themedius.ai'
+        cc_recipient = 'HR@themedius.ai'
+        
+        # Create the email
+        email = EmailMessage(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [recipient],
+            cc=[cc_recipient]
+        )
+        
+        # Attach the PNG file
+        file_path = os.path.join(settings.BASE_DIR, 'static/images/attachment.png')
+        email.attach_file(file_path)
+        
+        # Send the email
+        email.send(fail_silently=False)
+        
+        messages.success(request, 'Success!')
+        return redirect('subscribe')
+    
+    return render(request, 'subscriptions/home.html')
