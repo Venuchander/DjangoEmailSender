4f9fa3d0c66eef0ae236bd00cfbdf876221b7a63
Venuchander <venuchander1@gmail.com>
2024-07-13 19:53:10 +0530

    none

--- dj_email/settings.py
+++ dj_email/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,13 +53,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'dj_email.urls'
+load_dotenv()
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'venuchander1@gmail.com'
-EMAIL_HOST_PASSWORD = 'qngmsbmgfhoqimcx'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 import smtplib  
 with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as smtp:
